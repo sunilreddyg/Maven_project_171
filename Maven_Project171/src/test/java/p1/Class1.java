51fa28cc02cc8bb7a949b2bd6635e4311e24919f
Administrator <Administrator@192.168.1.2>
2019-12-20 10:37:14 +0530

    FirstUpdation

---
+++ Maven_Project171/src/test/java/p1/Class1.java
@@ -0,0 +1,25 @@
+package p1;
+
+import org.testng.annotations.Test;
+
+public class Class1
+{
+  @Test
+  public void tc001()
+  {
+	System.out.println("Tc001 executed");  
+  }
+  
+  
+  @Test
+  public void tc002()
+  {
+	System.out.println("Tc002 executed");  
+  }
+  
+  @Test
+  public void login()
+  {
+	System.out.println("login executed");  
+  }
+}
---
+++ Maven_Project171/src/test/java/p2/Class2.java
@@ -0,0 +1,32 @@
+package p2;
+
+import org.testng.annotations.Test;
+
+public class Class2 
+{
+
+	  @Test
+	  public void tc003()
+	  {
+		System.out.println("Tc003 executed");  
+	  }
+	  
+	  
+	  @Test
+	  public void tc004()
+	  {
+		System.out.println("Tc004 executed");  
+	  }
+	  
+	  @Test
+	  public void reg()
+	  {
+		System.out.println("reg executed");  
+	  }
+
+	  @Test
+	  public void email()
+	  {
+		System.out.println("email executed");  
+	  }
+}
